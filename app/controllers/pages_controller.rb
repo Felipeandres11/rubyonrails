e6d033f9f34dfa540d504bf3e82310055168f373
Felipe <felipeandres_uc@outlook.com>
2019-06-02 00:56:35 -0400

    tweet final

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+  
+  def index
+    
+  end
+
+  def about
+  end
+
+  def example 
+    @products = ["Producto1","Producto2","Producto3"]
+  end
+
+end
